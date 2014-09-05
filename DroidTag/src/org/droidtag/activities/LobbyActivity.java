0127eb3dacde53955e2370666a4a136bb7f1efbb
martijnschouwe <mschouwe@tripolis.com>
2014-09-05 13:35:35 +0200

    added icons , empty lobby activity

---
+++ DroidTag/src/org/droidtag/activities/LobbyActivity.java
@@ -0,0 +1,17 @@
+package org.droidtag.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.droidtag.R;
+
+/**
+ * Created by mschouwe on 9/4/14.
+ */
+public class LobbyActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lobby);
+    }
+}
